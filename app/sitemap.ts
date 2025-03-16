dfc5d1b833cb04929f345c1bb13c985f36f00c00
Kjetil Hårtveit <kjetil_runescape@hotmail.com>
2025-03-16 11:15:20 +0100

    initial

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+import fs from "fs/promises";
+import path from "path";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pathParts = ["content"];
+  /**
+   * Next.js 15.2.1 fails on the next line with the error:
+   *
+   * ```
+   * Error: ENOENT: no such file or directory, scandir '/var/task/content/author'
+   *     at async (.next/server/app/sitemap.xml/route.js:16:2327)
+   *     at async (.next/server/app/sitemap.xml/route.js:16:1342) {
+   * errno: -2,
+   * code: 'ENOENT',
+   * syscall: 'scandir',
+   *  path: '/var/task/content/author'
+   * }
+   * ```
+   */
+  const files = await fs.readdir(path.join(process.cwd(), ...pathParts));
+  const fileReads = files.map(async (fileName) => {
+    const readFile = await fs.readFile(
+      path.join(process.cwd(), ...pathParts, fileName),
+      "utf-8"
+    );
+    const meta = JSON.parse(readFile) as {
+      name: string;
+      slug: string;
+      position: string;
+    };
+    return {
+      fileNameSlug: path.parse(fileName).name,
+      meta,
+    };
+  });
+  const authors = await Promise.all(fileReads);
+
+  return authors.map((author) => ({
+    url: `https://example.com/authors/${author.meta.slug}`,
+  }));
+}
